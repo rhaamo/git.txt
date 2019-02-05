324f9e548a8fff4ce9eaba6f872d5424c262b365
Dashie <dashie@sigpipe.me>
2019-02-05 13:34:11 +0100

    Improve the new file javascript; Still getting that issue

--- static/js/GitxtNewPost.js
+++ static/js/GitxtNewPost.js
@@ -5,26 +5,29 @@
 $(document).on("click", "#add_new_form", function() {
     console.log("adding a new file fields");
     // Clone the file template
-    let new_file = $("div.files > div").last().clone();
+    let last_file = $("div.files > div").last();
+    let last_index = parseInt(last_file.data("id"));
 
-    // Empty the inputs
-    new_file.find("input[type='text']").val("");
-    new_file.find("textarea[type='text']").val("");
+    // Clone the template on end of form
+    let new_file = $("div.form.placeholders > div").clone();
 
     // Update the index
-    let index = parseInt(new_file.data("id"));
-    let newIndex = index + 1;
+    let newIndex = last_index + 1;
+
+    // Empty fields
+    new_file.find("input[type='text']").val("");
+    new_file.find("textarea[type='text']").val("");
 
     // This only works in debugger, not here, thank you JS
     //new_file.data("id", newIndex);
     new_file.attr("data-id", newIndex);
 
     new_file.attr("class", "form_" + newIndex + " gitxt_new_file");
-    new_file.find("label[for='file_"+index+"_filename']").attr("for", "file_"+newIndex+"_filename");
-    new_file.find("label[for='file_"+index+"_content']").attr("for", "file_"+newIndex+"_content");
+    new_file.find("label[for='file_INDEX_filename']").attr("for", "file_"+newIndex+"_filename");
+    new_file.find("label[for='file_INDEX_content']").attr("for", "file_"+newIndex+"_content");
 
-    new_file.find("input[id='file_"+index+"_filename']").attr("id", "file_"+newIndex+"_filename");
-    new_file.find("textarea[id='file_"+index+"_content']").attr("id", "file_"+newIndex+"_content");
+    new_file.find("input[id='file_INDEX_filename']").attr("id", "file_"+newIndex+"_filename");
+    new_file.find("textarea[id='file_INDEX_content']").attr("id", "file_"+newIndex+"_content");
 
     new_file.find("div[class='file_idx']").html("file " + newIndex);
 
